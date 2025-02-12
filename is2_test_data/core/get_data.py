f8b584c2c37c78047196873a71e3a2293ed8164c
Jessica Scheick <jbscheick@gmail.com>
2025-02-12 16:45:43 -0500

    switch to earthaccess instead of icepyx

--- is2_test_data/core/get_data.py
+++ is2_test_data/core/get_data.py
@@ -1,4 +1,4 @@
-import icepyx as ipx
+import earthaccess as ea
 
 '''
 FILE PURPOSE:
@@ -8,9 +8,8 @@ obtain a single granule of each data product
 """
 Tasks:
 - set up credentials in GH repo
-- fill in space/time for each product
-- switch to earthaccess?
-- download only first granule
+- figure out how to get an ATL11 file
+- see if QL products are available via earthaccess (or icepyx); currently no results from either, even with virtually no bounds
 - create script to get vars from file (use icepyx Vars module) and output to a reference file
 - from icepyx, get the vars from the file created herein
 
@@ -22,57 +21,57 @@ then...
 """
 
 ######## Product parameters to get #######
-# key=product_shortname; values=tuple([spatial],[temporal])
 # ATL01, ATL02, ATL04 are not included
-product_params = {
-    # "ATL03" : (),
-    "ATL06" : ([-55, 68, -48, 71],['2019-02-20','2019-02-28']),
-    # "ATL07" : (),
-    # "ATL07QL" : (),
-    # "ATL08" : (),
-    # "ATL09" : (),
-    # "ATL09QL" : (),
-    # "ATL10" : (),
-    # "ATL11" : ([-28.0, 62.0, -10.0, 68.0], []), #tracks=["1358"]),  # Get one specific track only; region is Iceland
-    # "ATL12" : (),
-    # "ATL13" : (),
-    # "ATL14" : (),
-    "ATL15" : ([-17.25, 80.7, -16.0, 81.0], ['2018-09-15', '2023-03-02']),
-    # "ATL16" : (),
-    # "ATL17" : (),
-    # "ATL19" : (),
-    # "ATL20" : (),
-    # "ATL21" : (),
-    # "ATL23" : (),
-}
+products = [
+            "ATL03",
+            "ATL06",
+            "ATL07",
+            # # "ATL07QL",
+            # "ATL08",
+            # "ATL09",
+            # # "ATL09QL",
+            # "ATL10",
+            # # "ATL11",
+            # "ATL12",
+            # "ATL13",
+            # "ATL14",
+            # "ATL15",
+            # "ATL16",
+            # "ATL17",
+            # "ATL19",
+            # "ATL20",
+            # "ATL21",
+            # "ATL23",
+            ]
 
 
 def download_single_product(product, spatial, temporal):
     """
     Download data for a single product
-
-    Args:
-        product (str): _description_
-        spatial (_type_): _description_
-        temporal (_type_): _description_
     """
 
-    query = ipx.Query(
-    product,
-    spatial,
-    temporal
+    query = ea.search_data(
+    short_name=product,
+    bounding_box=tuple(spatial),
+    temporal=tuple(temporal),
+    count=1
     )
 
     print(product)
-    print(len(query.avail_granules()))
+    print(len(query))
+
+    ea.login(strategy="netrc")
+    ea.download(query, "./data")
     
-    #TODO: only download the first granule
-    #TODO: consider using earthaccess directly?
-
-    # query.download_granules("./data", subset=False)
-
 
 def get_data():
+    
+    spatial = [-55, 68, -48, 71]
+    temporal = ['2019-02-20','2019-02-28']
 
-    for prod,params in product_params:
-        download_single_product(prod, params[0], params[1])
+    for prod in products:
+        download_single_product(prod, spatial, temporal)
+
+
+if __name__ == '__main__':
+    get_data()
